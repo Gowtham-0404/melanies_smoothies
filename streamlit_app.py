0d474bf72de8ad6d459506e06a8dd4e53ce06ff7
Gowtham-0404 <gowthamgopal1194@gmail.com>
2026-07-22 16:34:35 +0530

    Fix typo in subheader for nutritional information

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ if options:
     ingredients_string=''
     for ingredients in options:
         ingredients_string+=ingredients+' '
-      st.subheader( ingredients+ 'Nutrtional Information')
+        st.subheader( ingredients+ 'Nutrtional Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ingredients)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True);
     
